bab32b096713270977289ee48f45e664c636d0e5
JohnieCheng <johniecheng@gmail.com>
2026-06-23 17:25:39 +0800

    Initial Commit

---
+++ Recall/RecallApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecallApp.swift
+//  Recall
+//
+//  Created by Johnie Cheng on 2026/6/23.
+//
+
+import SwiftUI
+
+@main
+struct RecallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
